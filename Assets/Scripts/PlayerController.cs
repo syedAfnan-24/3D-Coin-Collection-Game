c63e389fb034fa2d4a789450704ac248ef846ba1
Qadri-Shadhili <91548766+Qadri-Shadhili@users.noreply.github.com>
2022-07-06 17:52:41 +0530

    my first 3d game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerControllerAndroid : MonoBehaviour
+{
+    public VariableJoystick variableJoystick;
+    public float speed;
+    public TextMeshProUGUI countText,winText;
+    private Rigidbody rb;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveHorizontal = variableJoystick.Horizontal;
+        float moveVertical = variableJoystick.Vertical;
+
+        Vector3 movement = new Vector3(moveHorizontal,0,moveVertical);
+
+        rb.AddForce(movement * speed);  
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Points: "+ count.ToString();
+            if(count >= 20)
+            {
+                winText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControllerAndroid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public TextMeshProUGUI countText,winText;
+    private Rigidbody rb;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,0,moveVertical);
+
+        rb.AddForce(movement * speed);  
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Points: "+ count.ToString();
+            if(count >= 20)
+            {
+                winText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
